ee939d46321373eff1bc9f1c42cc25e6f0461aeb
Prudhvi Raj Bhashampally <prudhvi@Mac.lan>
2026-02-15 01:51:44 -0500

    Before Generic website

--- next.config.ts
+++ next.config.ts
@@ -2,8 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: 'export',
-  basePath: process.env.NODE_ENV === 'production' ? '/AR-Visualizer' : '',
-  assetPrefix: process.env.NODE_ENV === 'production' ? '/AR-Visualizer' : '',
+  basePath: '/AR-Visualizer',
+  assetPrefix: '/AR-Visualizer',
   trailingSlash: true,
   images: {
     unoptimized: true,
